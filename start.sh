285f6cab120fcebf30a9157c5f9d5c7013687828
BigTitties <joey.boyfriend@gmail.com>
2022-03-26 18:25:43 +0700

    the chinese blog helped me

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/env bash
 
+## pip3 install torch==1.10.0+cu113 torchvision==0.11.1+cu113 torchaudio==0.10.0+cu113 -f https://download.pytorch.org/whl/cu113/torch_stable.html
+
 set -e
 
 echo "removing previous results"
