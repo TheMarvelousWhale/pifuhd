257f5a6db77b97c2898bf86144aeaac827fe9139
BigTitties <joey.boyfriend@gmail.com>
2022-03-26 17:18:54 +0700

    bug fix

--- start.sh
+++ start.sh
@@ -1,2 +1,23 @@
-python3 /lightweight-human-pose-estimation.pytorch/preprocessing.py /pifuhd/sample_images/sample.jpg
-python3 -m apps.simple_test --use_rect -r 512 -i ./sample_images
+#!/bin/env bash
+
+set -e
+
+echo "removing previous results"
+
+
+cd ../lightweight-human-pose-estimation.pytorch
+git fetch
+
+for file in $(ls ../pifuhd/sample_images/ | grep -E 'jpeg|jpg|png')
+do
+    echo "processing file $file..."
+    python preprocessing.py ../pifuhd/sample_images/"$file"
+done
+
+cd ../pifuhd
+git fetch
+python -m apps.simple_test --use_rect -r 256 -i ./sample_images
+
+mv ./results/pifuhd_final/recon/*.obj ../previous_results/
+rm ./results/pifuhd_final/recon/*
+rm ./sample_images/*
